d31e17e7f6d6275b0a20927c7969a8f96310b9fb
yanivsalzberg1 <yanivsalzberg@gmail.com>
2017-04-05 01:17:11 +0300

    second commit with buttons

--- clickBoard.js
+++ clickBoard.js
@@ -2,14 +2,14 @@ function playC4(){
     nodeC4.gain.value = .6;
     setTimeout(function(){
       nodeC4.gain.value = 0; /*The volume of the audio*/
-    }, 1000);
+    }, 100);
 };
 
 function playD4(){
     nodeD4.gain.value = .5;
     setTimeout(function(){
       nodeD4.gain.value = 0; /*The volume of the audio*/
-    }, 1000);
+    }, 100);
 };
 
 
@@ -17,7 +17,7 @@ function playE4(){
     nodeE4.gain.value = .4;
     setTimeout(function(){
       nodeE4.gain.value = 0; /*The volume of the audio*/
-    }, 1000);
+    }, 100);
 };
 
 
@@ -25,19 +25,19 @@ function playG4(){
     nodeG4.gain.value = .3;
     setTimeout(function(){
       nodeG4.gain.value = 0; /*The volume of the audio*/
-    }, 1000);
+    }, 100);
 };
 
 function playA4(){
     nodeA4.gain.value = .2;
     setTimeout(function(){
       nodeA4.gain.value = 0; /*The volume of the audio*/
-    }, 1000);
+    }, 100);
 };
 
 function playC5(){
     nodeC5.gain.value = .1;
     setTimeout(function(){
       nodeC5.gain.value = 0; /*The volume of the audio*/
-    }, 1000);
+    }, 100);
 };
---
+++ press.js
@@ -0,0 +1,11 @@
+Pressure.set('#center', {
+  change: function(force, event){
+    console.log(force);
+    var colorNum1 = Math.floor(force * 256);
+    var colorNum2 = 70;
+    var colorNum3 = 100;
+    console.log("rgb(" + colorNum1 + "," + colorNum2 + "," + colorNum3 + ")");
+    $("#center").css("background-color","rgb(" + colorNum1 + "," + colorNum2 + "," + colorNum3 + ")")
+    console.log(colorNum1);
+  }
+});
